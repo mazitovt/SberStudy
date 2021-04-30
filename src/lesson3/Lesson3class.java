3eba6b69ebcdfd8ee813c74f95a751067605d62c
mazitovt <timaz98@ya.ru>
2021-05-01 00:49:11 +0500

    lesson 3 commit

--- src/lesson3/Lesson3class.java
+++ src/lesson3/Lesson3class.java
@@ -3,5 +3,8 @@ package lesson3;
 public class Lesson3class {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
     }
 }
